1f750775542454b5872da059fc1fe1b37d2fc023
Kacper Odzimek <105165506+KacperOdzimek@users.noreply.github.com>
2026-02-24 22:10:11 +0100

    V0.5 - queue and review

--- include/riff/dynamic_array.h
+++ include/riff/dynamic_array.h
@@ -28,55 +28,51 @@
     Typedef
 */
 
-#ifndef darr
-    /*
-        Dynamic Array (darr)
-        Data structure, dynamically resizing, 
-        when it's size is too small to perform a push call
-        O(n) memory complexity
-    */
-    #define darr(inst) RIFF_INST(darr, inst)
-#endif
+// Dynamic Array (dyarr)
+// Data structure, dynamically resizing, 
+// when it's size is too small to perform a push call
+// O(n) memory complexity
+#define dyarr(inst) RIFF_INST(dyarr, inst)
 
-typedef struct darr(INSTANCE) {
+typedef struct dyarr(INSTANCE) {
     size_t  priv_size;
     size_t  priv_capc;
     STORED* priv_data;
-} darr(INSTANCE);
+} dyarr(INSTANCE);
 
 /*
     Zero / Destruction
 */
 
-RIFF_API(void) RIFF_INST(darr_zero, INSTANCE)(darr(INSTANCE)* tar) {
+// Makes unitialized memory proper 0-initialized empty dynamic array
+// Does not free anything
+#define dyarr_zero(inst) RIFF_INST(dyarr_zero, inst)
+
+RIFF_API(void) dyarr_zero(INSTANCE)(dyarr(INSTANCE)* tar) {
     tar->priv_size = 0;
     tar->priv_capc = 0;
     tar->priv_data = 0;
 }
 
-#ifndef darr_zero
-    // Makes unitialized memory proper 0-initialized empty dynamic array
-    // Does not free anything
-    #define darr_zero(inst) RIFF_INST(darr_zero, inst)
-#endif
+// Properly destroys given dynamic array
+// O(n) if destructor definied, O(1) otherwise
+#define dyarr_destroy(inst) RIFF_INST(dyarr_destroy, inst)
 
-RIFF_API(void) RIFF_INST(darr_destroy, INSTANCE)(darr(INSTANCE)* tar) {
+RIFF_API(void) dyarr_destroy(INSTANCE)(dyarr(INSTANCE)* tar) {
     DESTRUCTOR_LOOP(tar->priv_data, tar->priv_data + tar->priv_size);
     RIFF_FREE(tar->priv_data);
-    darr_zero(INSTANCE)(tar);
+    dyarr_zero(INSTANCE)(tar);
 }
-
-#ifndef darr_destroy
-    // Properly destroys given dynamic array
-    // O(n) if destructor definied, O(1) otherwise
-    #define darr_destroy(inst) RIFF_INST(darr_destroy, inst)
-#endif
 
 /*
     Memory
 */
 
-RIFF_API(int) RIFF_INST(darr_reserve, INSTANCE)(darr(INSTANCE)* arr, size_t capacity) {
+// Ensures dynamic array have at least given capacity (in total, not left)
+// May fail, O(1) else reallocation time complexity
+#define dyarr_reserve(inst) RIFF_INST(dyarr_reserve, inst)
+
+RIFF_API(int) dyarr_reserve(INSTANCE)(dyarr(INSTANCE)* arr, size_t capacity) {
     if (arr->priv_capc >= capacity) return SCC; // already have
     
     // realloc into bigger block
@@ -88,19 +84,18 @@ RIFF_API(int) RIFF_INST(darr_reserve, INSTANCE)(darr(INSTANCE)* arr, size_t capa
     return SCC;
 }
 
-#ifndef darr_reserve
-    // Ensures dynamic array have at least given capacity (in total, not left)
-    // May fail, O(1) else reallocation time complexity
-    #define darr_reserve(inst) RIFF_INST(darr_reserve, inst)
-#endif
+// Realoc array's memory into a block which tightly fit arrays elements. 
+// If already shrunk, no effect.
+// May fail, O(1) else reallocation time complexity
+#define dyarr_shrink_to_fit(inst) RIFF_INST(dyarr_shrink_to_fit, inst)
 
-RIFF_API(int) RIFF_INST(darr_shrink_to_fit, INSTANCE)(darr(INSTANCE)* arr) {
+RIFF_API(int) dyarr_shrink_to_fit(INSTANCE)(dyarr(INSTANCE)* arr) {
     size_t new_cap = arr->priv_size;
     if (arr->priv_capc == new_cap) return SCC; // already shrunk
 
     // fall to zero state
     if (new_cap == 0) {
-        darr_destroy(INSTANCE)(arr);
+        dyarr_destroy(INSTANCE)(arr);
         return SCC;
     }
     
@@ -113,68 +108,59 @@ RIFF_API(int) RIFF_INST(darr_shrink_to_fit, INSTANCE)(darr(INSTANCE)* arr) {
     return SCC;
 }
 
-#ifndef darr_shrink_to_fit
-    // Realoc array's memory into a block which tightly fit arrays elements. 
-    // If already shrunk, no effect.
-    // May fail, O(1) else reallocation time complexity
-    #define darr_shrink_to_fit(inst) RIFF_INST(darr_shrink_to_fit, inst)
-#endif
-
 /*
-    Getters
+    Query
 */
 
-RIFF_API(size_t) RIFF_INST(darr_size, INSTANCE)(darr(INSTANCE)* arr) {
+// Returns count of elements in the dynamic array
+#define dyarr_size(inst) RIFF_INST(dyarr_size, inst)
+
+RIFF_API(size_t) dyarr_size(INSTANCE)(dyarr(INSTANCE)* arr) {
     return arr->priv_size;
 }
 
-#ifndef darr_size
-    // Returns count of elements in the dynamic array
-    #define darr_size(inst) RIFF_INST(darr_size, inst)
-#endif
+// Proper way of obtaining count of elements in dynamic array
+#define dyarr_capacity(inst) RIFF_INST(dyarr_capacity, inst)
 
-RIFF_API(size_t) RIFF_INST(darr_capacity, INSTANCE)(darr(INSTANCE)* arr) {
+RIFF_API(size_t) dyarr_capacity(INSTANCE)(dyarr(INSTANCE)* arr) {
     return arr->priv_capc;
 }
-
-#ifndef darr_capacity
-    // Proper way of obtaining count of elements in dynamic array
-    #define darr_capacity(inst) RIFF_INST(darr_capacity, inst)
-#endif
 
 /*
     Access
 */
 
-RIFF_API(STORED*) RIFF_INST(darr_access, INSTANCE)(darr(INSTANCE)* arr) {
+// Returns pointer to first element of contiguous block of memory, the array
+// Only the first dyarr_size() amount of elements are initialized and valid
+// You can change given memory, but must keep object valid, as destructor (if provided)
+// will be called on them sooner or later
+// O(1)
+#define dyarr_access(inst) RIFF_INST(dyarr_access, inst)
+
+RIFF_API(STORED*) dyarr_access(INSTANCE)(dyarr(INSTANCE)* arr) {
     return (STORED*)arr->priv_data;
 }
 
-#ifndef darr_access
-    // Returns pointer to first element of contiguous block of memory, the array
-    // Only the first darr_size() amount of elements are initialized and valid
-    // You can change given memory, but must keep object valid, as destructor (if provided)
-    // will be called on them sooner or later
-    // O(1)
-    #define darr_access(inst) RIFF_INST(darr_access, inst)
-#endif
+// Returns pointer to first element of contiguous block of memory, the array
+// Only the first dyarr_size() amount of elements are initialized and valid
+// O(1)
+#define dyarr_const_access(inst) RIFF_INST(dyarr_const_access, inst)
 
-RIFF_API(const STORED*) RIFF_INST(darr_const_access, INSTANCE)(const darr(INSTANCE)* arr) {
+RIFF_API(const STORED*) dyarr_const_access(INSTANCE)(const dyarr(INSTANCE)* arr) {
     return (const STORED*)arr->priv_data;
 }
-
-#ifndef darr_const_access
-    // Returns pointer to first element of contiguous block of memory, the array
-    // Only the first darr_size() amount of elements are initialized and valid
-    // O(1)
-    #define darr_const_access(inst) RIFF_INST(darr_const_access, inst)
-#endif
 
 /*
     Operations
 */
 
-RIFF_API(int) RIFF_INST(darr_push, INSTANCE)(darr(INSTANCE)* arr, STORED value) {
+// This operation push new element into dynamic array
+// If succeeded dynamic array is now the owner of the object
+// May cause reallocation of dynamic array memory - watch out for your pointers
+// May fail, O(1) average
+#define dyarr_push(inst) RIFF_INST(dyarr_push, inst)
+
+RIFF_API(int) dyarr_push(INSTANCE)(dyarr(INSTANCE)* arr, STORED value) {
     if (arr->priv_size >= arr->priv_capc) {
         size_t new_cap = arr->priv_capc ? arr->priv_capc * 2 : 1;
         void* new_data = (STORED*)RIFF_REALLOC(arr->priv_data, new_cap * sizeof(STORED));
@@ -186,15 +172,13 @@ RIFF_API(int) RIFF_INST(darr_push, INSTANCE)(darr(INSTANCE)* arr, STORED value) 
     return SCC;
 }
 
-#ifndef darr_push
-    // This operation push new element into dynamic array
-    // If succeeded dynamic array is now the owner of the object
-    // May cause reallocation of dynamic array memory - watch out for your pointers
-    // May fail, O(1) average
-    #define darr_push(inst) RIFF_INST(darr_push, inst)
-#endif
+// Pops last element of the dynamic array
+// If out is null destructor (if provided) will be called on the poped object
+// Else object will be moved into *out
+// May fail (nothing to pop), O(1)
+#define dyarr_pop(inst) RIFF_INST(dyarr_pop, inst)
 
-RIFF_API(int) RIFF_INST(darr_pop, INSTANCE)(darr(INSTANCE)* arr, STORED* out) {
+RIFF_API(int) dyarr_pop(INSTANCE)(dyarr(INSTANCE)* arr, STORED* out) {
     if (arr->priv_size == 0) return ERR; // nothing to pop
     arr->priv_size--;
 
@@ -206,15 +190,16 @@ RIFF_API(int) RIFF_INST(darr_pop, INSTANCE)(darr(INSTANCE)* arr, STORED* out) {
     return SCC;
 }
 
-#ifndef darr_pop
-    // Pops last element of the dynamic array
-    // If out is null destructor (if provided) will be called on the poped object
-    // Else object will be moved into *out
-    // May fail (nothing to pop), O(1)
-    #define darr_pop(inst) RIFF_INST(darr_pop, inst)
-#endif
+// Pops out block of dynamic array elements, not only the last one
+// If out == NULL destructor is called on objects
+// Otherwise objects are copied into *out as described:
+// Object orders is preserved, eg. if dynamic array is {a, b, c, d}
+// calling dyarr_pop_many(arr, out, 3) would make array {a} and out {b, c, d}
+// Obviously out must be large enough array to store the elements
+// May fail (not enough elements to pop), O(amount)
+#define dyarr_pop_many(inst) RIFF_INST(dyarr_pop_many, inst)
 
-RIFF_API(int) RIFF_INST(darr_pop_many, INSTANCE)(darr(INSTANCE)* arr, STORED* out, size_t amount) {
+RIFF_API(int) dyarr_pop_many(INSTANCE)(dyarr(INSTANCE)* arr, STORED* out, size_t amount) {
     if (arr->priv_size < amount) return ERR; // not enough elements
 
     size_t first = arr->priv_size - amount;
@@ -226,28 +211,15 @@ RIFF_API(int) RIFF_INST(darr_pop_many, INSTANCE)(darr(INSTANCE)* arr, STORED* ou
     return SCC;
 }
 
-#ifndef darr_pop_many
-    // Pops out block of dynamic array elements, not only the last one
-    // If out == NULL destructor is called on objects
-    // Otherwise objects are copied into *out as described:
-    // Object orders is preserved, eg. if dynamic array is {a, b, c, d}
-    // calling darr_pop_many(arr, out, 3) would make array {a} and out {b, c, d}
-    // Obviously out must be large enough array to store the elements
-    // May fail (not enough elements to pop), O(amount)
-    #define darr_pop_many(inst) RIFF_INST(darr_pop_many, inst)
-#endif
+// Clear array, erases contained elements with destructor if provided, 
+// but does not reduce it capacity - combine with dyarr_shrink_to_fit() 
+// call afterwards to reduce array memory block
+#define dyarr_clear(inst) RIFF_INST(dyarr_clear, inst)
 
-RIFF_API(void) RIFF_INST(darr_clear, INSTANCE)(darr(INSTANCE)* arr) {
+RIFF_API(void) dyarr_clear(INSTANCE)(dyarr(INSTANCE)* arr) {
     DESTRUCTOR_LOOP(arr->priv_data, arr->priv_data + arr->priv_size);
     arr->priv_size = 0;
 }
-
-#ifndef darr_clear
-    // Clear array, erases contained elements with destructor if provided, 
-    // but does not reduce it capacity - combine with darr_shrink_to_fit() 
-    // call afterwards to reduce array memory block
-    #define darr_clear(inst) RIFF_INST(darr_clear, inst)
-#endif
 
 #undef DESTRUCTOR_LOOP
 
